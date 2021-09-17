ad93db29851bda579ac08e25b97e34b4ee221ada
Pawan Gautam <pawangtm2419@gmail.com>
2021-09-17 11:37:15 +0530

    Update

--- src/app/pipes/enginehours.pipe.ts
+++ src/app/pipes/enginehours.pipe.ts
@@ -1,17 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
-@Pipe({
-  name: 'enginehours'
-})
+@Pipe({ name: 'enginehours' })
 export class EnginehoursPipe implements PipeTransform {
 
   transform(value: any, ...args: any): any {
     var h = Math.floor(value / 3600);
     var m = Math.floor(value % 3600 / 60);
     var s = Math.floor(value % 3600 % 60);
-
-    return h +":"+ m +":"+ s; 
-
+    return h + ':' + m + ':' + s;
   }
 
 }
